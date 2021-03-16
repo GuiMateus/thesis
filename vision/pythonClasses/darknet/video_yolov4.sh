44e281993e98e0142b0a80ea29893d7794c10780
gus <gmateu16@student.aau.dk>
2021-03-16 12:42:34 +0100

    Second part of large commit

--- vision/pythonClasses/darknet/video_yolov4.sh
+++
@@ -1,6 +0,0 @@
-
-
-./darknet detector demo ./cfg/coco.data ./cfg/yolov4.cfg ./yolov4.weights test.webm -i 0 -thresh 0.25
-
-
-
--- vision/pythonClasses/deeplab/train_coco.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python3 train.py --backbone resnet --lr 0.01 --workers 4 --epochs 28 --batch-size 16 --gpu-ids 0 --checkname deeplab-resnet --eval-interval 1 --dataset coco
