6462bb83db41749a912e2734db87af63ff52dfcb
gus <gmateu16@student.aau.dk>
2021-06-18 18:51:27 +0200

    Fixed setup script.

--- setup.sh
+++ setup.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
 
 cd /opt
-sudo mkdir vision
-cd vision
-sudo mkdir weights
-cd weights
+sudo rm -f "download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204!2793&authkey=AGd1u1wsAuI7I9o"
+sudo wget --no-check-certificate "https://onedrive.live.com/download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204%212793&authkey=AGd1u1wsAuI7I9o"
 
-sudo mkdir deeplab
-cd deeplab
-sudo wget --no-check-certificate "https://onedrive.live.com/download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204%212745&authkey=APzYHuqFiJMXYkg"
-sudo mv "/opt/vision/weights/deeplab/download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204!2745&authkey=APzYHuqFiJMXYkg" "model_best.pth.tar"
+sudo tar xf "download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204!2793&authkey=AGd1u1wsAuI7I9o"
 
-cd ..
-sudo mkdir yolov4
-cd yolov4
-sudo wget --no-check-certificate "https://onedrive.live.com/download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204%212746&authkey=AOan3WK5lY1cZro"
-sudo mv "/opt/vision/weights/yolov4/download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204!2746&authkey=AOan3WK5lY1cZro" "pumps-v2.data"
-sudo wget --no-check-certificate "https://onedrive.live.com/download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204%212747&authkey=AGwJMFh6BCASi7E"
-sudo mv "/opt/vision/weights/yolov4/download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204!2747&authkey=AGwJMFh6BCASi7E" "yolov4-pumps-v2.cfg"
-sudo wget --no-check-certificate "https://onedrive.live.com/download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204%212748&authkey=AGM6m4QtG2CDItI"
-sudo mv "/opt/vision/weights/yolov4/download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204!2748&authkey=AGM6m4QtG2CDItI" "yolov4-pumps-v2_final.weights"
+sudo rm -f "download?cid=B7DCBFD795E0B204&resid=B7DCBFD795E0B204!2793&authkey=AGd1u1wsAuI7I9o"
 
 cd
 
